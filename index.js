70692eff32f4e73a51d9f8ff5306696ad0cf9205
tasha maina <tashabanks271@gmail.com>
2025-06-11 20:46:17 +0300

    Complete Arithmetic Lab Assignment

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
+// Arithmetic Lab solution
 
-// Write your code here
+// 1. Multiply num1 and num2 to get 62
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2; // 31 * 2 = 62
+
+// 2. Generate a random integer greater than 0
+const random = Math.floor(Math.random() * 100) + 1; // random > 0
+
+// 3. Get a remainder of 4 from num3 % num4
+const num3 = 16;
+const num4 = 6;
+const mod = num3 % num4; // 16 % 6 = 4
+
+// 4. Find the max number from a set that includes 20
+const max = Math.max(1, 5, 10, 15, 20);
